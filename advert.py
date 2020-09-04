430eeeac8c3e2a7d23c548f41accb6a336f2bd58
Joshua Coales <joshua@coales.co.uk>
2020-09-04 12:18:48 +0100

    Adding AdvertRecord class

--- advert.py
+++ advert.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 import enum
-from typing import Optional
+from datetime import datetime
+from typing import Optional, List
 
 
 class TransactionType(enum.Enum):
@@ -22,7 +23,31 @@ class Advert:
     link: str
     description: Optional[str]
 
+    @property
+    def advert_id(self) -> str:
+        return self.link
+
 
 class FullAdvert(Advert):
     bedrooms: int
     description: str
+
+
+@dataclass
+class AdvertRecord:
+    first_seen: datetime
+    advert_history: List[Advert]
+    _last_seen: Optional[datetime] = None
+    removed: bool = False
+
+    @property
+    def last_seen(self) -> datetime:
+        return self._last_seen
+
+    @last_seen.setter
+    def last_seen(self, last_seen: datetime):
+        self._last_seen = last_seen
+
+    @property
+    def record_id(self) -> str:
+        return self.advert_history[-1].advert_id
